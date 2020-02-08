0834a16d977bfb62a697d0c55f932355b679a59f
cladams0203 <cladams0203@gmail.com>
2020-02-07 22:16:57 -0500

    finished task router MVP complete

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 const resourceRouter = require('./resources/resourceRouter')
 const projectRouter = require('./projects/projectsRouter')
+const taskRouter = require('./tasks/tasksRouter')
 
 server.use(express.json())
 server.use(helmet())
@@ -12,6 +13,7 @@ server.use(cors())
 
 server.use('/api/resources', resourceRouter)
 server.use('/api/projects', projectRouter)
+server.use('/api/tasks', taskRouter)
 
 server.get('/', (req,res) => {
     res.status(200).json({message: 'welcome to the node db challenge'})
---
+++ tasks/tasksModal.js
@@ -0,0 +1,29 @@
+const db = require('../data/dbConfig')
+
+module.exports = {
+    find,
+    findByProjectId,
+    add
+}
+
+function find() {
+    return db('tasks')
+        .join('projects', 'project_id', 'projects.id')
+        .select('task_desc',
+                    'notes',
+                    'tasks.id',
+                    'tasks.task_complete',
+                    'tasks.project_id',
+                    'project_name',
+                    'project_desc'
+                 )
+}
+
+function findByProjectId(id) {
+    return db('tasks').where({project_id: id})
+}
+
+function add(task) {
+    return db('tasks').insert(task)
+        .then(([id]) => id)
+}
